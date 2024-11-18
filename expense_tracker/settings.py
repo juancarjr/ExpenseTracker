7bd3be60dd437b6b53b6f6fa15269e18a2e5a71c
Junior <juancarlosamarilla@proton.me>
2024-11-18 10:27:06 +0000

    fix: number formatting in the books list

--- expense_tracker/settings.py
+++ expense_tracker/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #A set of Django template filters for adding a “human touch” to data.
+    'django.contrib.humanize',
 
     #project apps
     'trackerAPI',
--- trackerAPI/views.py
+++ trackerAPI/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from rest_framework import generics
-from .models import Book
+from .models import Book, Category
 from .serializers import BookSerializer
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from .filters import BookFilter
@@ -12,9 +12,17 @@ def books_list(request):
     # books = Book.objects.all()
     books = BookFilter(
         request.GET,
-        queryset=Book.objects.all().select_related('category', 'publisher')
+        queryset = Book.objects.all().select_related('category', 'publisher')
     )
-    context = {'books': books}
+    total_expenses = {}
+    for category in Category.objects.all():
+        total_expenses[category] = Book.objects.get_total_expenses(category)
+
+    context = {'books': books,
+               'totals': total_expenses}
+
+    
+
     if request.htmx:
         return render(request, 'partials/books-container.html', context)
 
